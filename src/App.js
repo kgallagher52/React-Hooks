f6516f2b55b2824be3daf7772242a576fa054538
Keaton Gallagher <keatonrgallagher@Keatons-iMac.local>
2019-06-11 21:31:49 -0600

    adding more complex useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
+
+//Components
+import Notes from './components/Notes';
+
+//Style sheets
 import './App.css';
 
 const App = (props) => {
   const [count, setCount] = useState(props.count)
+  const [text, setText] = useState('count')
 
   return(
     <div>
-      <p>Count is {count}</p>
+      <Notes/>
+      <p>{text} is {count}</p>
+      <input value={text} placeholder="Change count" onChange={(e) => setText(e.target.value)}/>
       <button onClick={() => setCount(count + 1)}>increment count</button>
       <button onClick={() => setCount(count - 1)}>decrement count</button>
       <button onClick={() => setCount(props.count)}>reset count</button>
-
     </div>
   )
 }
---
+++ src/components/Notes.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+const Notes = () => {
+    const [notes, setNotes] = useState([])
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+
+
+    const addNote = (e) => {
+        e.preventDefault()
+        setNotes([
+            ...notes,
+            { title,body },
+        ])
+        setTitle('')
+        setBody('')
+
+    }
+
+    const removeNote = (title) => {
+        setNotes(notes.filter(n => (
+            n.title !== title
+        )))
+    }
+
+    return (
+        <div>
+            <h1>Notes Component</h1>
+            {notes.map(n => (
+                    <div key={n.title}>
+                        <h3>{n.title}</h3>
+                        <p>{n.body}</p>
+                        <button onClick={() => removeNote(n.title)}>Remove {n.title}</button>
+                    </div>
+            ))}
+            <p>Add Note!!!</p>
+            <form onSubmit={addNote}>
+                <input placeholder="TITLE" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                <input placeholder="BODY" value={body} onChange={(e) => setBody(e.target.value)}/>
+                <button>Add Note</button>
+            </form>
+        </div>
+    )
+}
+
+export default Notes
