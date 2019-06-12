b66bd567ec0c9c21d05e05261d203315edb7222f
Keaton Gallagher <keatonrgallagher@Keatons-iMac.local>
2019-06-11 20:53:15 -0600

    adding props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const App = () => {
-  const [count, setCount] = useState(0)
+const App = (props) => {
+  const [count, setCount] = useState(props.count)
 
   return(
     <div>
       <p>Count is {count}</p>
       <button onClick={() => setCount(count + 1)}>increment count</button>
       <button onClick={() => setCount(count - 1)}>decrement count</button>
+      <button onClick={() => setCount(props.count)}>reset count</button>
+
     </div>
   )
+}
+
+App.defaultProps = {
+  count:0
 }
 
 export default App;
